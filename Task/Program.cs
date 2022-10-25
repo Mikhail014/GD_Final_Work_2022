8c88215cabfd542cf501648823ce5103030e8076
Mikhail014 <msemin704@gmail.com>
2022-10-25 18:33:24 +0300

    Настройка кодировки. Добавлены три массива

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,1 +1,5 @@
-﻿
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+string[] textArray1 = new string[]{"hello", "2", "world", ":-)"};
+string[] textArray2 = new string[]{"1234", "1567", "-2", "computer scince"};
+string[] textArray3 = new string[]{"Russia", "Denmark", "Kazan"};
