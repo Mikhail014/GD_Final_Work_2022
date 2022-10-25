8b2b2ead0273b4717735bd7eb7bfffe2b6027c02
Mikhail014 <msemin704@gmail.com>
2022-10-25 18:37:51 +0300

    Добавлен метод, возвращающий массив из строк, длина которых меньше, либо равна 3-м символам

--- Task/Program.cs
+++ Task/Program.cs
@@ -3,3 +3,14 @@
 string[] textArray1 = new string[]{"hello", "2", "world", ":-)"};
 string[] textArray2 = new string[]{"1234", "1567", "-2", "computer scince"};
 string[] textArray3 = new string[]{"Russia", "Denmark", "Kazan"};
+
+string[] GetTextArrayMaxWithThreeSymbols(string[] arr)
+{
+    string[] newArr = new string[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        char[] symbols = arr[i].ToCharArray();
+        if (symbols.Length <= 3) newArr[i] = arr[i];
+    }
+    return newArr.Where(x => x != null).ToArray();
+}
